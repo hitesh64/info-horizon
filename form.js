c1013072082ecccba415a7f9ec19d3fab5f6fd01
hitesh64 <hitesh64@gmail.com>
2025-08-27 13:21:55 +0530

    Replace repository contents with local folder

--- form.js
+++
@@ -1,6 +0,0 @@
-function openFormPage() {
-    document.getElementById("userForm").style.right = "0";
-}
-function closeFormPage() {
-    document.getElementById("userForm").style.right = "-100%";
-}
